c5dc1ee49fb584305f08e183628a8286e849aacf
Scott O'Neal <scottmod@gmail.com>
2014-10-04 17:29:56 -0400

    Added some animations to main and nav, fun times!

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,21 +1,19 @@
 'use strict';
 
 angular.module('bbfabApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    var base = '/assets/images/';
-    $scope.slides = [
-      {
-        image: base+'me.jpg',
-        title: 'Scott'
-      },
-      {
-        image: base+'will.jpg',
-        title: 'Will'
-      },
-      {
-        image: base+'guy.jpg',
-        title: 'Guy'
-      }
-    ];
+  .controller('MainCtrl', function ($scope, $http, $timeout) {
+   
+   // Delayed Shows 
+    $timeout(function() {
+      $scope.countdown = true;
+    }, 750);
+    
+    $timeout(function() {
+      $scope.instagram = true;
+    }, 1000);
+    
+    $timeout(function() {
+      $scope.twitter = true;
+    }, 1750);
 
   });
