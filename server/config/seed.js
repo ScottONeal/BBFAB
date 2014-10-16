e68eec919bd4dccfc6fb74e435a4c6925ff9fb2c
Scott O'Neal <scottmod@gmail.com>
2014-10-16 10:12:22 -0400

    Change Seed file to be more app specific.

--- server/config/seed.js
+++ server/config/seed.js
@@ -10,9 +10,28 @@ var User = require('../api/user/user.model');
 User.find({}).remove(function() {
   User.create({
     provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
+    name: "Scott O'Neal",
+    city: "Bowie",
+    state: "MD",
+    email: 'scott@scott.com',
+    password: 'pass',
+    raised: 35
+  }, {
+    provider: 'local',
+    name: 'Tim Barker',
+    city: "Grand Rapids",
+    state: "MI",
+    email: 'tim@tim.com',
+    password: 'pass',
+    raised: 50
+  }, {
+    provider: 'local',
+    name: 'Matt Norris',
+    city: "Bowie",
+    state: "MD",
+    email: 'matt@matt.com',
+    password: 'pass',
+    raised: 45
   }, {
     provider: 'local',
     role: 'admin',
